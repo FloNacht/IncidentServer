162740e68e370360da8df3d616be52ea7f82a1ab
D062794 <florian.nachtigall@sap.com>
2016-12-20 17:54:33 +0100

    Add UserRestController unit tests

--- webservice/src/main/java/com/imservice/WebserviceApplication.java
+++ webservice/src/main/java/com/imservice/WebserviceApplication.java
@@ -26,30 +26,25 @@ import com.model.*;
 	}
 	
 
-	@Bean
-	CommandLineRunner init(UserRepository userRepository, IncidentRepository incidentRepository) {
-		return (evt) -> Arrays.asList("628123,621278,612893,589231,783242,R.Hartmann,F.Becker,231328,657912".split(","))
-				.forEach(a -> {
-					
-					if (a.matches("[0-9]+")) {
-						
-						User user = userRepository.save(new User(a, "password"));
-						
-						Random r = new Random();
-						char c = (char)(r.nextInt(26) + 'A');
-						
-						incidentRepository.save(new Incident(user, "Dreckig","Haus " + c, "Unter der Decke",
-								"Hier ist was dreckig", "image dir"));
-						incidentRepository.save(new Incident(user, "Kaputt", "Haus " + c, "Heizung",
-								"Hier ist was kaputt", "image dir"));
-					} else {
-						userRepository.save(new User(a, "password", Role.FACULTY));
-					}
-				});
-	}
-
-//	 @Override
-//     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//         return application.sources(WebserviceApplication.class);
-//     }
+//	@Bean
+//	CommandLineRunner init(UserRepository userRepository, IncidentRepository incidentRepository) {
+//		return (evt) -> Arrays.asList("628123,621278,612893,589231,783242,R.Hartmann,F.Becker,231328,657912".split(","))
+//				.forEach(a -> {
+//					
+//					if (a.matches("[0-9]+")) {
+//						
+//						User user = userRepository.save(new User(a, "password"));
+//						
+//						Random r = new Random();
+//						char c = (char)(r.nextInt(26) + 'A');
+//						
+//						incidentRepository.save(new Incident(user, "Dreckig","Haus " + c, "Unter der Decke",
+//								"Hier ist was dreckig", "image dir"));
+//						incidentRepository.save(new Incident(user, "Kaputt", "Haus " + c, "Heizung",
+//								"Hier ist was kaputt", "image dir"));
+//					} else {
+//						userRepository.save(new User(a, "password", Role.FACULTY));
+//					}
+//				});
+//	}
 }
